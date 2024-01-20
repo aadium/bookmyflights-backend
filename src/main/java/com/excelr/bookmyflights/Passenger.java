a011602b07fd17ce4c3a1fc113f148b602a22864
chavvatharun <127178096+ChavvaTharun@users.noreply.github.com>
2024-01-20 13:32:48 +0530

    Create Passenger.java

---
+++ src/main/java/com/excelr/bookmyflights/Passenger.java
@@ -0,0 +1,69 @@
+package com.excelr.bookmyflights;
+
+import java.util.UUID;
+import org.springframework.data.annotation.Id;
+
+
+public class Passenger {
+	@Id
+	
+	private UUID id ;
+	
+	private String name;
+	
+	private long number;
+	
+	private String address;
+	
+	
+	
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public long getNumber() {
+		return number;
+	}
+
+	public void setNumber(long number) {
+		this.number = number;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Passenger() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Passenger(UUID id, String name, long number, String address) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.number = number;
+		this.address = address;
+	}
+	
+	
+	
+	
+	
+}
