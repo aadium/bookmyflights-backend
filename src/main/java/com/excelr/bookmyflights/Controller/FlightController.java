597d3bff5e2b306bd363cc0e2aec0d8b0e5782a8
Aadi Umrani <47914987+aadium@users.noreply.github.com>
2024-01-28 13:26:00 +0300

    got the backend working

--- src/main/java/com/excelr/bookmyflights/Controller/FlightController.java
+++ src/main/java/com/excelr/bookmyflights/Controller/FlightController.java
@@ -6,10 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.excelr.bookmyflights.Service.FlightService;
 import com.excelr.bookmyflights.model.Flight;
 
+@RestController
+@RequestMapping("/flights")
 public class FlightController {
 
 	
