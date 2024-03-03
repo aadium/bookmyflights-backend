c815888890b3f16a3614112a8f6109c142bcad75
Aadi Umrani <aadi.p.um@gmail.com>
2024-03-03 19:03:15 +0300

    changed mapping

--- src/main/java/com/excelr/bookmyflights/Controller/TicketController.java
+++ src/main/java/com/excelr/bookmyflights/Controller/TicketController.java
@@ -4,13 +4,18 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.excelr.bookmyflights.service.TicketService;
+
+import jakarta.websocket.server.PathParam;
+
 import com.excelr.bookmyflights.exceptions.ResourceNotFoundException;
 import com.excelr.bookmyflights.model.Ticket;
 
@@ -43,9 +48,9 @@ public class TicketController {
 		return tk;
 	}
 
-	@GetMapping("/deleteTicket/{id}")
+	@DeleteMapping("/deleteTicket/{id}")
 	@PreAuthorize("hasAuthority('ROLE_USER')")
-	public Ticket deleteTicket(@RequestBody String id) {
+	public Ticket deleteTicket(@PathVariable String id) {
 		Ticket tk = service.deleteTicket(id);
 		if (tk == null) {
 			throw new ResourceNotFoundException("Ticket not found");
